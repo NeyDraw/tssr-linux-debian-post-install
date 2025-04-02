4915f3727d995eedd2cbc98691e9b68bc7256071
NeyDraw <genu0zz37@gmail.com>
2025-04-02 11:44:40 +0200

    Update SRV-AD.ps1

--- SRV-AD.ps1
+++ SRV-AD.ps1
@@ -12,23 +12,8 @@ $CSVFile = "C:\Users\Administrator\users.csv" ??????
 Rename-Computer -NewName $NewComputerName -Force
 Restart-Computer -Force
 
-# 2. Configurer une IP fixe
-$Interface = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
-New-NetIPAddress -InterfaceIndex $Interface.ifIndex -IPAddress $StaticIP -PrefixLength 24 -DefaultGateway $Gateway
-Set-DnsClientServerAddress -InterfaceIndex $Interface.ifIndex -ServerAddresses $DNSServer
-
-# Vérifier l'IP
-Write-Host "Vérification de la configuration réseau :"
-Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.IPAddress -eq $StaticIP}
-
 # 3. Installer le rôle AD DS
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 # 4. Promouvoir en contrôleur de domaine
 Install-ADDSForest -DomainName $DomainName -SafeModeAdministratorPassword $SafeModePassword -Force
-
-# 5. Création des OU
-Import-Module ActiveDirectory
-New-ADOrganizationalUnit -Name "Administrateurs" -Path "DC=mondomaine,DC=local"
-New-ADOrganizationalUnit -Name "Utilisateurs" -Path "DC=mondomaine,DC=local"
-New-ADOrganizationalUnit -Name "Techniciens" -Path "DC=mondomaine,DC=local"
