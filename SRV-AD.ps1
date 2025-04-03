3d5ae4a06439acf34cf818c7d18f00cafa056157
NeyDraw <genu0zz37@gmail.com>
2025-04-03 09:38:04 +0200

    Update SRV-AD.ps1

--- SRV-AD.ps1
+++ SRV-AD.ps1
@@ -1,9 +1,9 @@
 # Définition des variables
 $NewComputerName = "SRV-AD"
-$StaticIP = "192.168.1.200"
+$StaticIP = "10.0.0.200"
 $SubnetMask = "255.255.255.0"
-$Gateway = "192.168.1.1"
-$DNSServer = "192.168.1.200"
+$Gateway = "10.0.0.1"
+$DNSServer = "10.0.0.200"
 $DomainName = "SRV.Draw"
 $SafeModePassword = ConvertTo-SecureString "SrvDraw37*" -AsPlainText -Force
 $CSVFile = "C:\Users\Administrator\users.csv" ??????
